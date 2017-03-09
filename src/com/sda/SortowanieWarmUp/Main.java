0f05b17aeeb05ff6dc3e5311c9afe1c387edced3
grovas <arzcir@gmail.com>
2017-03-09 17:49:33 +0100

    first commit

---
+++ src/com/sda/SortowanieWarmUp/Main.java
@@ -0,0 +1,20 @@
+package com.sda.SortowanieWarmUp;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] tab = {4,7,8,3,5,4};
+        int temp = 0;
+
+        for (int i=0; i<tab.length; i+=2) {
+            temp = tab[i];
+            tab[i] = tab[i+1];
+            tab[i+1] = temp;
+            }
+        System.out.println(Arrays.toString(tab));
+    }
+}
+
